59a68865ab97166a740d10b2621539e8e18fb4e8
GitHub Copilot <github-copilot@users.noreply.github.com>
2026-05-10 02:15:27 +0530

    Commit current code files

---
+++ 02_JavaScript_Identifiers.js
@@ -0,0 +1,19 @@
+var V  = 10
+var _V = 20
+var $V = 30
+var v1 = 40
+var v = 50
+var de = 60
+//var 1v = 70  // var 1v = 70  // Invalid identifier, cannot start with a digit
+//var -T = 80  // Invalid identifier, cannot start with a hyphen
+// var &Z = 90  // Invalid identifier, cannot contain special characters like '&'
+var name = "Mubasheer"
+var Name = "Ahamed"
+
+console.log(de)
+console.log(V)
+console.log(_V)
+console.log($V)
+console.log(v1)
+console.log(v); console.log(name)
+console.log(Name)
