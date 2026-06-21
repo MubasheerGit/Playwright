a149c71610934a34deba45672242d13aed7c6189
GitHub Copilot <github-copilot@users.noreply.github.com>
2026-06-21 21:25:49 +0530

    Save changes

---
+++ 07_Functions/01_functions.js
@@ -0,0 +1,19 @@
+function name() {
+
+    console.log("hi you have called the name function");
+
+}
+name();
+
+function add(a, b, c) {
+
+    let sum = a + b + c;
+    return sum
+}
+
+console.log(add(3, 5, 9));
+
+const add2 = n=> n-2
+
+let r= add2(5);
+console.log(r);
